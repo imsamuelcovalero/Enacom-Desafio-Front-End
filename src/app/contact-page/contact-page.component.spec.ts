2729304c456129e8770eb0bbaf533136f9acfb4d
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2023-06-23 20:27:14 -0300

    test(Contact): Cria os testes do componente e do serviço.

--- src/app/contact-page/contact-page.component.spec.ts
+++ src/app/contact-page/contact-page.component.spec.ts
@@ -1,4 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { CopyService } from '../services/copy.service';
 
 import { ContactPageComponent } from './contact-page.component';
 
@@ -8,9 +15,11 @@ describe('ContactPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContactPageComponent ]
+      declarations: [ContactPageComponent],
+      imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatSnackBarModule, ToastrModule.forRoot(), BrowserAnimationsModule],
+      providers: [CopyService]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +30,40 @@ describe('ContactPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should make the contact form invalid when empty', () => {
+    expect(component.contactForm.valid).toBeFalsy();
+  });
+
+  it('should make the contact form valid when correctly filled', () => {
+    component.contactForm.controls['name'].setValue("John Doe");
+    component.contactForm.controls['email'].setValue("johndoe@test.com");
+    component.contactForm.controls['subject'].setValue("Test Subject");
+    component.contactForm.controls['message'].setValue("Test Message");
+    expect(component.contactForm.valid).toBeTruthy();
+  });
+
+  it('should call the submitForm method when contact form is submitted', () => {
+    spyOn(component, 'submitForm');
+    component.contactForm.controls['name'].setValue("John Doe");
+    component.contactForm.controls['email'].setValue("johndoe@test.com");
+    component.contactForm.controls['subject'].setValue("Test Subject");
+    component.contactForm.controls['message'].setValue("Test Message");
+
+    component.submitForm();
+    expect(component.submitForm).toHaveBeenCalled();
+  });
+
+  it('should call the copyEmail method when the copy email button is clicked', () => {
+    spyOn(component, 'copyEmail');
+    component.copyEmail();
+    expect(component.copyEmail).toHaveBeenCalled();
+  });
+
+  it('should call the copyWhatsApp method when the copy WhatsApp button is clicked', () => {
+    spyOn(component, 'copyWhatsApp');
+    component.copyWhatsApp();
+    expect(component.copyWhatsApp).toHaveBeenCalled();
   });
 });
--- src/app/services/copy.service.spec.ts
+++ src/app/services/copy.service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { CopyService } from './copy.service';
 
 describe('CopyService', () => {
   let service: CopyService;
+  let snackBar: MatSnackBar;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [MatSnackBarModule]
+    });
     service = TestBed.inject(CopyService);
+    snackBar = TestBed.inject(MatSnackBar);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should copy text and show a message', (done) => {
+    const text = 'Test text';
+    const message = 'Message test';
+    const snackBarSpy = spyOn(snackBar, 'open').and.stub();
+    const clipboardSpy = spyOn(navigator.clipboard, 'writeText').and.returnValue(Promise.resolve());
+
+    service.copyText(text, message);
+
+    setTimeout(() => {
+      expect(clipboardSpy).toHaveBeenCalledWith(text);
+      expect(snackBarSpy).toHaveBeenCalledWith(message, 'Close', {
+        duration: 2000,
+      });
+      done();
+    }, 1000);
   });
 });
