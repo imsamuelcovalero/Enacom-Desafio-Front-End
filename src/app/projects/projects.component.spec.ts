361e9aa85fff803437216f82a5f08e26e7a42f10
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2023-06-23 16:26:04 -0300

    test(Projects): Finaliza os testes, agora no componente principal e no serviço de comments.

--- src/app/projects/projects.component.spec.ts
+++ src/app/projects/projects.component.spec.ts
@@ -1,16 +1,61 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { Input, Directive } from '@angular/core';
 import { ProjectsComponent } from './projects.component';
+import { ProjectsService } from '../services/projects.service';
+import { IProject } from '../interfaces/project.interface';
+
+// Mock service
+class MockProjectsService {
+  getProjects() {
+    return [
+      {
+        id: 1,
+        name: 'Test Project 1',
+        title: 'Test Title',
+        description: 'Test Description',
+        image: 'test-image.jpg',
+        skills: ['Test Skill'],
+        linkApp: 'test-app',
+        linkGitHub: 'test-github'
+      },
+      {
+        id: 2,
+        name: 'Test Project 2',
+        title: 'Test Title',
+        description: 'Test Description',
+        image: 'test-image.jpg',
+        skills: ['Test Skill'],
+        linkApp: 'test-app',
+        linkGitHub: 'test-github'
+      }
+    ] as IProject[];
+  }
+}
+
+// Mock component
+@Directive({
+  selector: 'app-carousel'
+})
+class MockCarouselComponent {
+  @Input()
+  projects!: IProject[];
+}
 
 describe('ProjectsComponent', () => {
   let component: ProjectsComponent;
   let fixture: ComponentFixture<ProjectsComponent>;
+  let mockProjectsService: MockProjectsService;
 
   beforeEach(async () => {
+    mockProjectsService = new MockProjectsService();
+
     await TestBed.configureTestingModule({
-      declarations: [ ProjectsComponent ]
-    })
-    .compileComponents();
+      declarations: [ProjectsComponent, MockCarouselComponent],
+      providers: [
+        { provide: ProjectsService, useValue: mockProjectsService }
+      ]
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +66,17 @@ describe('ProjectsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch projects on init', () => {
+    const projectsSpy = spyOn(mockProjectsService, 'getProjects').and.callThrough();
+    component.ngOnInit();
+    expect(projectsSpy).toHaveBeenCalled();
+  });
+
+  it('should pass projects to CarouselComponent', () => {
+    const carouselElement = fixture.debugElement.query(By.directive(MockCarouselComponent));
+    expect(carouselElement).toBeTruthy();
+    expect(carouselElement.componentInstance.projects).toBe(component.projects);
   });
 });
--- src/app/services/comments.service.spec.ts
+++ src/app/services/comments.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CommentsService } from './comments.service';
+import { IComment } from '../interfaces/project.interface';
 
 describe('CommentsService', () => {
   let service: CommentsService;
@@ -8,9 +8,31 @@ describe('CommentsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(CommentsService);
+
+    // Limpa localStorage antes de cada teste
+    localStorage.clear();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get comments for a project', () => {
+    const comment: IComment = { projectId: 1, body: 'Test comment', author: 'Test author' };
+
+    // Salva um comentário no localStorage
+    localStorage.setItem('myPortfolioComments', JSON.stringify([comment]));
+
+    const comments = service.getComments(1);
+    expect(comments).toContain(comment);
+  });
+
+  it('should add a comment', () => {
+    const comment: IComment = { projectId: 2, body: 'Another test comment', author: 'Another test author' };
+
+    service.addComment(comment);
+    const commentsJSON = localStorage.getItem('myPortfolioComments');
+    const comments = commentsJSON ? JSON.parse(commentsJSON) : [];
+    expect(comments).toContain(comment);
   });
 });
